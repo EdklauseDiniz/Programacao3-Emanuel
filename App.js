79e100b82b5ca96136d9b986980186a69f2ff457
EdklauseDiniz <edklausezinho@gmail.com>
2026-02-12 08:51:06 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Alert,
+  Platform,
+} from "react-native";
+import { Button, TextInput } from "react-native-web";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Edklause Diniz 1</Text>
-      <StatusBar style="auto" />
-
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+        }}
+      />
+      <Text style={{margin: 15}}></Text>
+      <Text>Email</Text>
+      <TextInput style={styles.input}></TextInput>
+      <Text>Senha</Text>
+      <TextInput style={styles.input}></TextInput>
+      <View style={styles.button}>
+        <Button title="Logar" color="#d84545"></Button>
+        <Text style={{margin: 15}}></Text>
+        <Button title="Cadastre-se" color="#d84545"></Button>
+      </View>
+      <Text style={{margin: 15}}>esqueceu a senha</Text>
     </View>
   );
 }
@@ -15,8 +38,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1458d6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tinyLogo: {
+    width: 250,
+    height: 250,
+  },
+  button: {
+    width: 200,
+    marginTop: 50,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
